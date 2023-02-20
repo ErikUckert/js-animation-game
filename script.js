c34917b1dcf62466c02f0ed7bae3136490491aa3
Erik Uckert <erik.uckert@avl.com>
2023-02-20 15:54:22 +0100

    init

---
+++ script.js
@@ -0,0 +1,102 @@
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white'
+
+    class Player {
+        constructor(game){
+            this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
+            this.speedX = 0;
+            this.speedY = 0;
+            this.dX = 0;
+            this.dY = 0;
+            this.speedModifier = 5;
+        }
+        draw(context){
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY);
+            context.lineTo(this.game.mouse.x, this.game.mouse.y);
+            context.stroke();
+        }
+        update(){
+            this.dX = this.game.mouse.x - this.collisionX;
+            this.dY = this.game.mouse.y - this.collisionY;
+            const distance = Math.hypot(this.dY, this.dX);
+            if (distance > this.speedModifier){
+                this.speedX = this.dX / distance || 0;
+                this.speedY = this.dY / distance || 0;
+            } else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
+        }
+    }
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+            // event listeners
+            canvas.addEventListener('mousedown', (e) => {
+                // using an arrow function -- (e) => {} -- here allows me to access
+                // the properties of the
+                // Game class because arrow functions remember their declarion postition
+                // a normal function like -- function(e){} --
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            })
+            canvas.addEventListener('mouseup', (e) => {
+
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            })
+            canvas.addEventListener('mousemove', (e) => {
+                if (this.mouse.pressed){
+                    this.mouse.x = e.offsetX;
+                    this.mouse.y = e.offsetY;
+                }
+            })
+        }
+        render(context){
+            this.player.draw(context);
+            this.player.update();
+        }
+    }
+
+    const game = new Game(canvas);
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate);
+    }
+
+    animate();
+})
